0f45161f1cb062a2821a8580f320fafffaaea89a
AndreyMena <andrey.menaespinoza@ucr.ac.cr>
2020-10-14 12:26:35 -0600

    12:25

--- Examenes/AndreyMenaEspinozaC04740/Instruccion.java
+++ Examenes/AndreyMenaEspinozaC04740/Instruccion.java
@@ -14,6 +14,18 @@ public class Instruccion
      */
     public Instruccion()    
     { 
+        Random randomizador = new Random(); 
+        int asignador;
+        asignador = randomizador.nextInt(5);
+        asignador = asignador+1;
+        identificador = asignador;
+    }
+    
+    public String
+    
+    /*
+    public int asignador() 
+    {
         String instruccion1 = "1-No se mueva";
         String instruccion2 = "2-Avance 4 lugares";
         String instruccion3 = "3-Brinque a la próxima fila";
@@ -24,10 +36,28 @@ public class Instruccion
         asignador = randomizador.nextInt(5);
         asignador = asignador+1;
         
+        if (asignador == 1) {
+            identificador = instruccion1;
+        }
+        if (asignador == 2) {
+            identificador = instruccion2;
+        }
+        if (asignador == 3) {
+            identificador = instruccion3;
+        }
+        if (asignador == 4) {
+            identificador = instruccion4;
+        }
+        if (asignador == 5) {
+            identificador = instruccion5;
+        } 
+        return identificador;
+        
+       
+       return asignador;
+       
     }
-    
-    
-    
+       */
     
 
 }
--- Examenes/AndreyMenaEspinozaC04740/Tablero.java
+++ Examenes/AndreyMenaEspinozaC04740/Tablero.java
@@ -20,6 +20,19 @@ public class Tablero
         filas = opcion;
         columnas = opcion;
         tablero = new Instruccion[filas][columnas];
+        mensaje = new String[filas][columnas];
+        this.llenarMatriz();
+    }
+    
+    public void llenarMatriz(){
+        for (int i= 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                Instruccion instruccion = new Instruccion();
+                tablero[i][j] = instruccion;
+                mensaje[i][j] = instruccion;
+                
+            }
+        }
     }
 
 }
