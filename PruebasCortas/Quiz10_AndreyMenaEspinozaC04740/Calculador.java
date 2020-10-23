c568a98ad8888e11bc4ede614612c2d13e6cf3dc
AndreyMena <andrey.menaespinoza@ucr.ac.cr>
2020-10-23 07:17:28 -0600

    7:17

---
+++ PruebasCortas/Quiz10_AndreyMenaEspinozaC04740/Calculador.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Calculador here.
+ * 
+ * @author (AndreyMenaEspinoza C04740) 
+ * @version (1.1)
+ */
+public class Calculador
+{
+    /**
+     * Constructor for objects of class Calculador
+     */
+    public int Calculador(int[] laLista)
+    {
+        int[] lista = laLista;
+        int valor = 0;
+        int filas = lista.length();
+        for (int i ; i< filas; i++) {
+        }
+        
+        return valor;
+    }
+}
