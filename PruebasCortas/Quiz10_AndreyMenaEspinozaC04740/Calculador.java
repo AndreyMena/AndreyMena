0d093089469dfd123c716ad64a256e114fdd3ab6
AndreyMena <andrey.menaespinoza@ucr.ac.cr>
2020-10-23 07:42:33 -0600

    7:43

--- PruebasCortas/Quiz10_AndreyMenaEspinozaC04740/Calculador.java
+++ PruebasCortas/Quiz10_AndreyMenaEspinozaC04740/Calculador.java
@@ -10,16 +10,21 @@ public class Calculador
     /**
      * Constructor for objects of class Calculador
      */
-    public int Calculador(int[] laLista)
+    public int Calculador(int[] laLista,int i)
     {
         int[] lista = laLista;
-        int valor = 0;
-        int filas = lista.length;
-        int i = 0; 
-        if (lista[i] <= 0) {
+        int valor = 0;  
+        int subSolucion;
+        if (lista[i] <= 0 && i < lista.length) {
             return 0;
+        }else{
+            if (lista[i] > 0 && i < lista.length) {
+                valor = lista[i]^2;
+                i++;
+                valor = valor+Calculador(lista,i);
+            }
         }
         
-        return valor;
+
     }
 }
